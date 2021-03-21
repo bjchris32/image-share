b3a7ed34ea6eb4ddc5742702614baee9ee8ab07d
Chris Shao <bjchris32@gmail.com>
2021-03-21 23:57:47 +0800

    feat(carrier wave initializer, media uploader): generate the media uploader and set up the config

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,7 @@ class Image
   include Mongoid::Document
   field :title, type: String
   field :authentication_token, type: String
+  mount_uploader :media, MediaUploader
 
   validates_uniqueness_of :authentication_token
 end
