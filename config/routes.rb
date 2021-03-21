223ace9599d5f3c758a694b5f2403ff7262f6761
Chris Shao <bjchris32@gmail.com>
2021-03-22 00:04:24 +0800

    feat(GridfsController, routes): add the GridfsController to serve the frontend request for media content

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :images
+
+  get '/uploads/grid/image/media/:id/:filename' => 'gridfs#serve', :constraints  => { :filename => /[0-z\.]+/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
